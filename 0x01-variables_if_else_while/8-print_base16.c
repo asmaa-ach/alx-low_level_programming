60089211ee1533c87fccc34e72ee1287f287d282
asmaa-ach <asmaa2009achoubir@gmail.com>
2023-07-16 13:24:35 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+  *main:entry point
+  *Description: prints all the numbers of base 16 in lowercase
+  *Return:always 0
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (n = 65; n <= 70; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
+
