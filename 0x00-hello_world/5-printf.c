2c9102e8e09a11bc4da5abbffb1060861d6a7083
asmaa-ach <asmaa2009achoubir@gmail.com>
2023-07-16 16:35:27 -0700

    hello_world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * output: program that prints using printf
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
